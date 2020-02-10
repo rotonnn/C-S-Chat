428ccfc667b295eca414d69c57e02aa4fd9cca5f
763534650@qq.com <763534650@qq.com>
2020-02-10 20:10:36 +0800

    2-10

--- chat/client.py
+++ chat/client.py
@@ -5,7 +5,7 @@ from setting import *
 import random
 
 
-def connect(p_serv,s_cli):
+def connect(s_cli,p_serv):
     rlst = [s_cli,p_serv]
     wlst = []
    
@@ -13,11 +13,14 @@ def connect(p_serv,s_cli):
         rd, wd, ex = select(rlst, wlst, rlst)
         for r in rd:
             if r is s_cli:
-                data = r.recv(1024)
-                print(data.decode('utf-8'))
+                try:
+                    data = r.recv(1024)
+                    print(data.decode('utf-8'))
+                except Exception:
+                    continue
+                
             else:
                 conn, addr = r.accept()
-                
                 data = conn.recv(1024)
                 s_cli.send(data)
                 conn.close()
@@ -26,14 +29,14 @@ if __name__ == "__main__":
     name=input("input your nickname: ")
     s_cli = sock_cli(SOCK_SERV)
     s_cli.send(bytes('{} connection...'.format(name),'utf-8'))
-    
+ 
     while True:
         PORT = random.randint(5000, 10000)
         if PORT not in PORTS:break
     P_ADDR=('127.0.0.1',PORT)
     p_serv = sock_serv(P_ADDR)
 
-    t = threading.Thread(target=connect, args=(p_serv, s_cli))
+    t = threading.Thread(target=connect, args=(s_cli,p_serv))
     t.setDaemon(True)
     t.start()
 
@@ -45,9 +48,8 @@ if __name__ == "__main__":
             p_serv.close()
             break
         if not data:
-            continue
+            break
         else:
-           
             p_cli = sock_cli(P_ADDR)
             p_cli.send(bytes(name+' : '+data, 'utf-8'))
             p_cli.close()
--- chat/server.py
+++ chat/server.py
@@ -3,8 +3,12 @@ import socket,sys,threading
 from select import select
 from setting import *
 
-def listen(s_serv, p_serv):
-    rlst = [s_serv, p_serv]
+ 
+            
+if __name__ == "__main__":
+    s_serv = sock_serv(SOCK_SERV)
+   
+    rlst = [s_serv]
     wlst = []
     print("listening...")
     
@@ -12,46 +16,19 @@ def listen(s_serv, p_serv):
         rd, wd, ex = select(rlst, wlst, rlst)
         for h in rd:
             if h is s_serv:
+                print('s-SERV')
                 conn, addr = h.accept()
-                print(conn)
+                print(conn.recv(1024))
                 rlst.append(conn)
-            elif h is p_serv:
-                conn, addr = w.accept()
-                data = conn.recv(1024)
-                print('data',data)
-                for i in rlst[2:]:
-                    i.send(data)
-                conn.close()
-
-            
             else:
+                print('s-CLI')
+               
                 data = h.recv(1024)
-                print(data.decode('utf-8'
-                ), end=' ')
-                for c in rlst[2:]:
-                    c.send(data)
-          
-            
-if __name__ == "__main__":
-    s_serv = sock_serv(SOCK_SERV)
-    p_serv = sock_serv(PIPE_SERV)
+                if data == b'':
+                    print("user leave")
+                    rlst.remove(h)
+                    continue
+                for i in rlst[1:]:
+                    i.send(data)
     
-    t = threading.Thread(target=listen, args=(s_serv, p_serv))
-    t.setDaemon(True)
-    t.start()
-    
-    while True:
-        try:
-            data = sys.stdin.readline()
-        except KeyboardInterrupt:
-            s_serv.close()
-            p_serv.close()
-            break
-        if not data:
-            continue
-        else:
-            print("inputed ",data)
-            p_cli = sock_cli(PIPE_SERV)
-            p_cli.send(bytes('manager : '+ data, 'utf-8'))
-            p_cli.close()
-        
+               
